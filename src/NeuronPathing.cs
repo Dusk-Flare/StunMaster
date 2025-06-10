702d6c10634c6972644daaeeb382420bee8fbdad
DuskFlare13 <166411211+Dusk-Flare@users.noreply.github.com>
2025-06-09 23:09:56 -0300

    Add files via upload

---
+++ src/NeuronPathing.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static SSOracleSwarmer;
+
+namespace StunMaster
+{
+    internal static class NeuronPathing
+    {
+        internal static MovementMode Controlled = new MovementMode("Controlled", register: true);
+
+        internal static void NeuronControll(SSOracleSwarmer self, Player controller)
+        {
+            if (self.grabbedBy.Count > 0 && self.grabbedBy[0].grabber is Player && controller.slugcatStats.name.value == "stunmaster")
+            {
+                float baseSpeed = 5f;
+                float accel = 0.9f;
+
+                Vector2 inputDir = Vector2.zero;
+                if (controller.input[0].analogueDir.magnitude > 0f)
+                {
+                    inputDir = controller.input[0].analogueDir.normalized;
+                }
+                else if (controller.input[0].x != 0 || controller.input[0].y != 0)
+                {
+                    inputDir = new Vector2(controller.input[0].x, controller.input[0].y).normalized;
+                }
+
+                if (inputDir != Vector2.zero)
+                {
+                    self.mode = Controlled;
+                    self.color = new Vector2((float)UnityEngine.Random.Range(0, 3) / 2f, (UnityEngine.Random.value < 0.75f) ? 0f : 1f);
+                    self.travelDirection = inputDir;
+                    self.firstChunk.vel += inputDir * accel;
+
+                    if (self.firstChunk.vel.magnitude > baseSpeed)
+                        self.firstChunk.vel = self.firstChunk.vel.normalized * baseSpeed;
+
+                    self.rotation = Mathf.Atan2(inputDir.y, inputDir.x);
+                }
+                else if (self.mode == Controlled)self.mode = SSOracleSwarmer.MovementMode.Swarm;
+            }
+        }
+
+    }
+}
---
+++ src/Plugin.cs
@@ -0,0 +1,80 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace StunMaster;
+
+[BepInPlugin("com.author.stunmaster", "Stun Master", "0.1.0")]
+sealed class Plugin : BaseUnityPlugin
+{
+    public static new ManualLogSource Logger;
+    public static Dictionary<Player, float> stunCooldowns = [];
+    bool IsInit;
+
+    public void OnEnable()
+    {
+        Logger = base.Logger;
+        On.Player.Update += Player_Update;
+        On.RainWorld.OnModsInit += OnModsInit;
+        On.SSOracleBehavior.NewAction += CustomIteratorDialogue;
+        On.SSOracleSwarmer.Update += NeuronOverride;
+    }
+
+
+    private void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+
+        if (IsInit) return;
+        IsInit = true;
+    }
+    private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
+    {
+        orig(self, eu);
+        if (!stunCooldowns.ContainsKey(self)) stunCooldowns[self] = 0f;
+        if (stunCooldowns.ContainsKey(self) && stunCooldowns[self] > 0f) stunCooldowns[self]--;
+
+        if (self.slugcatStats.name.value == "stunmaster")
+        {
+            if (self.input[0].jmp && self.input[0].pckp && (!self.input[1].jmp || !self.input[1].pckp))
+            {
+                bool isArena = self.room?.game != null && self.room.game.IsArenaSession;
+                if (isArena)
+                {
+                    StunPower.TriggerArenaStun(self, 7, 3, 2);
+                }
+                else
+                {
+                    StunPower.TriggerStoryStun(self, 7, 2);
+                }
+            }
+        }
+    }
+
+    private void NeuronOverride(On.SSOracleSwarmer.orig_Update orig, SSOracleSwarmer self, bool eu)
+    {
+        orig(self, eu);
+        Player player = self.grabbedBy.Count > 0 ? self.grabbedBy[0].grabber as Player : null;
+        NeuronPathing.NeuronControll(self, player);
+    }
+
+
+
+
+    private void CustomIteratorDialogue(On.SSOracleBehavior.orig_NewAction orig, SSOracleBehavior self, SSOracleBehavior.Action action)
+    {
+        orig(self, action);
+        return; // Disable all iterator dialogue
+        var metBefore = self.oracle?.room?.game?.GetStorySession?.saveState?.miscWorldSaveData?.SSaiThrowOuts;
+        if (self.oracle.room.game.StoryCharacter.value != "stunmaster" || metBefore > 0) orig(self, action);
+        StunDialog.MeetPebbles(self, action);
+        orig(self, action);
+    }
+}
